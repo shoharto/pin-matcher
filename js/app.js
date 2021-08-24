939d0baa4e25db9a2923715d1ad8219103cd5025
shoharto <bayes@graduate.utm.my>
2021-08-24 23:16:28 +0800

    Use event bubble to create calculator and clear

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
 function getPin() {
     const pin = Math.round(Math.random() * 10000);
-    const pinString = pin + '';
+    const pinString = pin + "";
     if (pinString.length == 4) {
         return pin;
-    }
-    else {
+    } else {
         //console.log('call again 3 digit pin', pin);
         return getPin();
     }
@@ -12,6 +11,21 @@ function getPin() {
 
 function generatePin() {
     const pin = getPin();
-    document.getElementById('display-pin').value = pin;
+    document.getElementById("display-pin").value = pin;
     console.log(pin);
 }
+
+document.getElementById("key-pad").addEventListener("click", function (event) {
+    const number = event.target.innerText;
+    const calcInput = document.getElementById("typed-numbers");
+
+    if (isNaN(number)) {
+        if (number == "C") {
+            calcInput.value = "";
+        }
+    } else {
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber;
+    }
+});
